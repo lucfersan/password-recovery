636f6f29846d2fcb8469600c566db3b01633bb50
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-06-05 19:41:09 -0300

    feat: added environment variables using dotenv-flow

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata';
+import { config } from 'dotenv-flow';
+config({ silent: true });
+
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import { routes } from './routes';
